1c08265891f07e273d2bf8cc54e1a95b6469cedf
“Florent <f.losada@hotmail.fr>
2019-09-18 11:25:49 +0200

    permission location

--- app/src/main/java/com/dtc/theendormap/MainActivity.kt
+++ app/src/main/java/com/dtc/theendormap/MainActivity.kt
@@ -1,15 +1,57 @@
 package com.dtc.theendormap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import timber.log.Timber
+
+// requestcode -> jeton qui rapelle
+private const val REQUEST_PERMISSION_LOCATION_LAST_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateLastLocation()
+    }
 
-        Timber.i("Hello from MainActivity")
+    private fun updateLastLocation() {
+        Timber.d("updateLocation()")
+        // vérifier si on a la permission pour vérifier la location
+        if (!checkLocationPermission()) {
+            return
+        }
+    }
+
+    // vérifie si on a la permission
+    private fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // demande de permission (dialog)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSION_LOCATION_LAST_LOCATION
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+       // si pas reçu de résultats
+        if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        when (requestCode) {
+            REQUEST_PERMISSION_LOCATION_LAST_LOCATION -> updateLastLocation()
+
+        }
     }
 }
