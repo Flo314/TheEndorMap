b1e83094e233f6cf19de196d5a835ab1f66df754
“Florent <f.losada@hotmail.fr>
2019-09-19 14:59:54 +0200

    personalize style map

--- app/src/main/java/com/dtc/theendormap/MainActivity.kt
+++ app/src/main/java/com/dtc/theendormap/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MapStyleOptions
 import timber.log.Timber
 
 // requestcode -> jeton qui rapelle
@@ -60,6 +61,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        // configurer la carte et chargé le fichier raw qui contient le style de la map
+        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.maps_style))
     }
 
     private fun handleLocationData(locationData: LocationData) {
