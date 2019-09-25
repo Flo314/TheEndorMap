a29b98889daeba8ed291b833d5a77230bb31a92b
“Florent <f.losada@hotmail.fr>
2019-09-25 09:10:39 +0200

    test

--- app/src/test/java/com/dtc/theendormap/MapViewModelTest.kt
+++ app/src/test/java/com/dtc/theendormap/MapViewModelTest.kt
@@ -3,7 +3,7 @@ package com.dtc.theendormap
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.dtc.theendormap.map.MapUiState
 import com.dtc.theendormap.map.MapViewModel
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,12 +14,22 @@ class MapViewModelTest {
     @JvmField
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    // test sur la séquence d'état
     @Test
-    fun loadPoiTriggersLoading() {
+    fun `load Poi triggers loading success`() {
         val viewModel = MapViewModel()
         val observer = viewModel.getUiState().testObserver()
         viewModel.loadPois(0.0, 0.0)
 
-        Assert.assertEquals(MapUiState.Loading, observer.observedValues[0])
+        assertEquals(MapUiState.Loading, observer.observedValues[0])
+    }
+
+    @Test
+    fun `load POIs with invalid coordinates error`() {
+        val viewModel = MapViewModel()
+        val observer = viewModel.getUiState().testObserver()
+        viewModel.loadPois(-91.0, -181.0)
+
+        assertTrue(observer.observedValues[0] is MapUiState.Error)
     }
 }
